2988ecb98632dc030d4929823c5640e79b961a1a
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 04:19:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.any? {|cheese| array.include?(cheese)}
+  if cheese_types.any? {|cheese| array.include?(cheese)}
+    return cheese
+  end
 end
