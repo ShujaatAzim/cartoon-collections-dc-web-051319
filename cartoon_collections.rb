8603ff7357fd220374e179bfbd74e5d1027cfa2c
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 04:24:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.any? {|cheese| array.find (cheese_types)}
+  array.any? {|cheese| array.find(cheese_types)}
     return cheese
 end
